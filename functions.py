bdd71bb4a4cac2f0718bef5a754457270d9caca1
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-11-21 20:57:38 +0000

    all functions defenitions

--- functions.py
+++ functions.py
@@ -2,64 +2,64 @@
 import requests
 import json
 
-#get user basic info (picture,username, strengths)
-userinfo = []
 
-response = requests.get("https://torre.bio/api/bios/dfrodriguezor")
+def getuserinfo(user_public_id):
+    #get user basic info (picture,username, strengths)
 
-userpicture = response.json()['person'].get('picture')
+    userinfo = []
 
-username = response.json()['person'].get('name')
-
-userinfo.append(username)
-userinfo.append(userpicture)
-
-
-userstrengths = response.json()['strengths']
-
-for names in userstrengths:
-    strengthname = names['name']
-    userinfo.append(strengthname)
-
-print(userinfo)
-
-
-#get user connections ids 
-idslists = []
-usercontacts = requests.get("https://bio.torre.co/api/people/dfrodriguezor/connections")
-
-
-contactsids = usercontacts.json()
-
-for i in range(len(contactsids)):
-    cont = contactsids[i]['person']['publicId']
-    idslists.append(cont)
-
-
-# get all connections strenghts
-
-users_info = []
-
-for y in idslists:
-    all_user_strenghts = []
-    response = requests.get("https://torre.bio/api/bios/{}".format(y))
+    response = requests.get("https://torre.bio/api/bios/{}".format(user_public_id))
 
     userpicture = response.json()['person'].get('picture')
 
     username = response.json()['person'].get('name')
 
-    all_user_strenghts.append(username)
-    all_user_strenghts.append(userpicture)
+    userinfo.append(username)
+    userinfo.append(userpicture)
+
 
     userstrengths = response.json()['strengths']
 
     for names in userstrengths:
         strengthname = names['name']
-        all_user_strenghts.append(strengthname)
-    users_info.append(all_user_strenghts)
+        userinfo.append(strengthname)
+
+    return set(userinfo)
+
+    
 
 
-# intersection beetween user all contacts strengths
+def publics_ids_list(user_public_id):
+#get user connections public_ids 
+    idslists = []
+    usercontacts = requests.get("https://bio.torre.co/api/people/{}/connections".format(user_public_id))
 
-    for all in users_info:
-        print(intersection(userinfo,users_info))
+
+    contactsids = usercontacts.json()
+
+    for i in range(len(contactsids)):
+        cont = contactsids[i]['person']['publicId']
+        idslists.append(cont)
+    print(idslists)   
+    return idslists
+
+
+
+
+
+def all_connections_strenghts(user_public_id):
+# get all connections strenghts
+    listofids = publics_ids_list(user_public_id)
+    users_info = []
+
+    for y in listofids:
+        users_info.append(getuserinfo(y))
+    print(len(users_info))
+    print(users_info)
+    return users_info
+
+#
+
+print(type(getuserinfo('dfrodriguezor')))
+
+print(type(all_connections_strenghts('dfrodriguezor')))
